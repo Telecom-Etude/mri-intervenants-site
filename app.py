572400985d41f8a7430ff28926f59faa232d46bb
stroumpf <nicolas.besson@telecom-paris.fr>
2024-10-27 17:21:57 +0100

    Base pour les tests d'envoi d'HTML

--- app.py
+++ app.py
@@ -2,10 +2,34 @@ import json
 from flask import Flask, render_template, request, redirect, make_response, send_from_directory
 import database
 
+with open('secret.txt','r',encoding='utf-8') as f:
+    secret = f.readline()
+
 app = Flask(__name__)
 @app.route("/")
 def index():
     return app.send_static_file("html/main.html")
+
+@app.route("/ping",methods=["GET","POST"])
+def ping():
+    try:
+        data = request.get_json()
+        print(data)
+    except:
+        print("Pas de donnée dans la request")
+    return "pong"
+
+@app.route('/new',methods=["POST"])
+def new_mri():
+    try:
+        data = request.get_json()
+        if data["secret"] != secret:
+            return "Unauthorized"
+        print("Accepté")
+    except:
+        print("Pas de donnée dans la request")
+        return "Error"
+    return "OK"
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -1,5 +1,18 @@
-import os
+import datetime
 import pymongo
+import json
+from bs4 import BeautifulSoup
 from pymongo import MongoClient
-from bson.objectid import ObjectId
+#from bson.objectid import ObjectId
 
+uri = "mongodb://localhost:27017/MRI"
+client = MongoClient(uri)
+database=client["MRI"]
+collection=database["MRI"]
+
+def insert_mri(mri):
+    now=datetime.datetime.now(tz=datetime.timezone.utc)
+    mri_soup = BeautifulSoup(mri)
+    title=mri_soup.find("h1").text
+    print(title)
+    
